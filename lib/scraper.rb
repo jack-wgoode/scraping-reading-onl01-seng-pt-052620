c32f5b828afea7ac01ad378c8f7e37cd3feb6160
Jack Goode <jack.w.goode@icloud.com>
2020-07-02 00:09:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,9 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 puts doc.css(".headline-26OIBN").text 
 
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G") 
+
+
 
 class Course 
   attr_accessor :title
